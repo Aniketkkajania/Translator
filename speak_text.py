4896783dd16674ed4f446dc7495e5abe53961d0b
ANIKET KAJANIA <Aniketkajaniya428@gmail.com>
2023-06-19 22:17:29 +0530

    changed some files

--- speak_text.py
+++ speak_text.py
@@ -2,13 +2,15 @@ from gtts import gTTS
 import os
 import pygame.mixer
 import pygame
+from pygame import mixer
 
-pygame.mixer.init()
+pygame.mixer.init(44100, -16,2,2048)
+
 
 
 def speak_text(txt, dest):
         tts = gTTS(text=txt, lang=dest)
         tts.save("output.mp3")
-        sound = pygame.mixer.Sound("output.mp3")
+        sound = mixer.Sound("output.mp3")
         sound.play()
         os.remove("output.mp3")
